fd1b5d2a62b6516f4d3fff23272f61884e209268
Nicholas Disalvio <102550462+ndisalvio3@users.noreply.github.com>
2025-02-18 19:39:20 -0500

    Update install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -4,7 +4,7 @@
 wget -qO - https://apt.hostmc4free.com/debian/archive.key | sudo tee /etc/apt/trusted.gpg.d/hostmc4free.asc
 
 # Add the repository
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/hostmc4free.asc] https://apt.hostmc4free.com/debian ./" | sudo tee /etc/apt/sources.list.d/hostmc4free.list
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/hostmc4free.asc] https://apt.hostmc4free.com/ universal-apt main" | sudo tee /etc/apt/sources.list.d/hostmc4free.list
 
 # Update package lists
 sudo apt update
